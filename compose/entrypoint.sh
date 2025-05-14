ed594241bcd7cba16c47f85a03f2108c57164c49
Lukasz Zmudzinski <lukasz@zmudzinski.me>
2025-05-14 09:39:16 +0200

    Replace poetry with uv

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -4,26 +4,26 @@ set -e
 if [ "$1" = 'runserver' ]; then
     shift
     cd src
-    exec python -m pelican -lr content -s core/settings.py -o output -b 0.0.0.0 --ignore-cache $@
+    uv run python -m pelican -lr content -s core/settings.py -o output -b 0.0.0.0 --ignore-cache $@
 elif [ "$1" = 'lint' ]; then
     shift
     OPTS=${@:-'.'}
-    echo "-- ruff lint --" && ruff check $OPTS || EXIT=$?
-    echo "-- ruff format --" && ruff format --check $OPTS || EXIT=$?
+    echo "-- ruff lint --" && uv run ruff check $OPTS || EXIT=$?
+    echo "-- ruff format --" && uv run ruff format --check $OPTS || EXIT=$?
     MYPY_OPTS=${@:-'src/'}
-    echo "-- mypy --" && mypy $MYPY_OPTS || EXIT=$?
+    echo "-- mypy --" && uv run mypy $MYPY_OPTS || EXIT=$?
     exit ${EXIT:-0}
 elif [ "$1" = 'fmt' ]; then
     shift
     OPTS=${@:-'.'}
-    echo "-- ruff lint --" && ruff check --fix $OPTS
-    echo "-- ruff format --" && ruff format $OPTS
+    echo "-- ruff lint --" && uv run ruff check --fix $OPTS
+    echo "-- ruff format --" && uv run ruff format $OPTS
     exit 0
 elif [ "$1" = 'test' ]; then
     shift
     OPTS=${@:-'src/'}
-    echo "-- bandit --" && bandit -r $OPTS
-    echo "-- pytest --" && pytest --cov=src --cov-report=json
+    echo "-- bandit --" && uv run bandit -r $OPTS
+    echo "-- pytest --" && uv run pytest --cov=src --cov-report=json
 fi
 
 exec "$@"
